ee5e564f9d9caf9525d9d6be06f202a9516b7909
Simon Brulhart <simon@brulhart.me>
2019-08-25 19:06:54 +0200

    Clean up

--- presentation_mode.py
+++ presentation_mode.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-# Dependencies: aiohttp, dbussy
-
-# Actions
-# start/stop or blocking: systemd-inhibit
-# loop: org.freedesktop.ScreenSaver.SimulateUserActivity
-# start/stop or blocking: org.freedesktop.Notifications.Inhibit
-# start/stop: slack do not disturb
-# brightness 100%
 
 import asyncio
 import logging
@@ -30,43 +22,34 @@ async def wait_forever():
         await asyncio.sleep(3600)
 
 
-async def wait_shielded():
-    logger = logging.getLogger("wait_shielded")
-    while True:
-        try:
-            await wait_forever()
-        except asyncio.CancelledError:
-            logger.debug("Catch CancelledError")
-            pass
-
-
-async def throw_cancelled():
-    await asyncio.sleep(1)
-    raise asyncio.CancelledError
-
-
 async def inhibit_notifications():
     conn = await dbussy.Connection.bus_get_async(DBUS.BUS_SESSION, private=False)
-    msg = dbussy.Message.new_method_call(
-        destination="org.freedesktop.Notifications",
-        path="/org/freedesktop/Notifications",
-        iface="org.freedesktop.Notifications",
-        method="Inhibit",
-    ).append_objects("ssa{sv}", __file__, "Presentation mode", {})
-    reply = await conn.send_await_reply(msg)
-    await wait_forever()
+    try:
+        msg = dbussy.Message.new_method_call(
+            destination="org.freedesktop.Notifications",
+            path="/org/freedesktop/Notifications",
+            iface="org.freedesktop.Notifications",
+            method="Inhibit",
+        ).append_objects("ssa{sv}", os.path.basename(__file__), "Presentation mode", {})
+        reply = await conn.send_await_reply(msg)
+        await wait_forever()
+    finally:
+        conn.close()
 
 
 async def keep_screen_active():
     conn = await dbussy.Connection.bus_get_async(DBUS.BUS_SESSION, private=False)
-    msg = dbussy.Message.new_method_call(
-        destination="org.freedesktop.ScreenSaver",
-        path="/org/freedesktop/ScreenSaver",
-        iface="org.freedesktop.ScreenSaver",
-        method="Inhibit",
-    ).append_objects("ss", __file__, "Presentation mode")
-    reply = await conn.send_await_reply(msg)
-    await wait_forever()
+    try:
+        msg = dbussy.Message.new_method_call(
+            destination="org.freedesktop.ScreenSaver",
+            path="/org/freedesktop/ScreenSaver",
+            iface="org.freedesktop.ScreenSaver",
+            method="Inhibit",
+        ).append_objects("ss", os.path.basename(__file__), "Presentation mode")
+        reply = await conn.send_await_reply(msg)
+        await wait_forever()
+    finally:
+        conn.close()
 
 
 async def slack_dnd():
@@ -76,114 +59,51 @@ async def slack_dnd():
         logger.warning("SLACK_API_TOKEN is empty, skip Slack DnD")
         return
     client = slack.WebClient(token=slack_token, run_async=True)
-    # while True:
-    # try:
-    # await asyncio.sleep(10)
-    # except asyncio.CancelledError:
-    # logger.debug("Catching CancelledError")
-    # pass
+    await client.dnd_setSnooze(num_minutes=15)
     try:
         while True:
-            # await client.dnd_setSnooze(num_minutes=15)
-            await asyncio.sleep(10)
-    # except asyncio.CancelledError:
+            await asyncio.sleep(10 * 60)
+            await client.dnd_setSnooze(num_minutes=15)
     finally:
         logger.info('Disabling "Do Not Disturb"')
-        await asyncio.sleep(1)
-        logger.info("after sleep")
-        # await asyncio.wait_for(client.dnd_endSnooze(), 5)
-        # await asyncio.wait_for(asyncio.sleep(10), 5)
+        await client.dnd_endSnooze()
 
 
-async def main_routine():
-    loop = asyncio.get_running_loop()
-    tasks = [
-        inhibit_notifications(),
-        keep_screen_active(),
-        slack_dnd(),
-        # wait_shielded(),
-    ]
-    tasks = [asyncio.create_task(coro) for coro in tasks]
-    # gathering_task = asyncio.gather(*tasks, return_exceptions=True)
-    current_task = asyncio.current_task()
-
-    # def cancel_tasks():
-        # logger.debug("Cancelling tasks...")
-
-        # current_task.cancel()
-        # gathering_task.cancel()
-        # for task in tasks:
-        # task.cancel()
-
-    # loop.add_signal_handler(signal.SIGTERM, cancel_tasks)
-    # loop.add_signal_handler(signal.SIGINT, cancel_tasks)
-
-    # asyncio.gather(*tasks)
-    try:
-        await asyncio.gather(*tasks, return_exceptions=False)
-        # await gathering_task
-        # pass
-    # except asyncio.CancelledError:
-    finally:
-        # logger.info("Waiting for tasks to clean up... (press CTRL+C to force quit)")
-        # await gathering_task
-        # await asyncio.wait_for(
-            # asyncio.gather(*tasks, return_exceptions=True), timeout=CLEANUP_SECONDS
-        # )
-        loop.close()
-        # loop.remove_signal_handler(signal.SIGTERM)
-        # loop.remove_signal_handler(signal.SIGINT)
-    logger.debug("End of main_routine")
+async def run_tasks():
+    tasks = [inhibit_notifications(), keep_screen_active(), slack_dnd()]
+    await asyncio.gather(*tasks, return_exceptions=False)
 
 
-async def main2():
-    logger = logging.getLogger('main2')
+async def main_coro():
+    logger = logging.getLogger("main")
     loop = asyncio.get_running_loop()
 
-    task = asyncio.create_task(main_routine())
+    main_task = asyncio.create_task(run_tasks())
     current_task = asyncio.current_task()
-    
+
     def cancel_tasks():
         logger.debug("Cancelling tasks...")
-        # logger.debug((current_task, asyncio.current_task()))
         current_task.cancel()
-        # task.cancel()
 
-    def coucou(word=''):
-        logger.debug(f"COUCOU {word}")
-    
-    signals = (signal.SIGTERM, signal.SIGINT)
-    # signals = ()
+    def force_quit():
+        logger.debug("Stopping the loop by force...")
+        loop.stop()
+
+    signals = (signal.SIGTERM, signal.SIGINT, signal.SIGHUP)
     for sig in signals:
         loop.add_signal_handler(sig, cancel_tasks)
-    
-    # loop.add_signal_handler(signal.SIGINT, cancel_tasks)
-    # loop.add_signal_handler(signal.SIGINT, coucou)
-    # loop.add_signal_handler(signal.SIGINT, coucou, '2')
-    # loop.add_signal_handler(signal.SIGINT, coucou, '3')
 
     try:
         await wait_forever()
+    except asyncio.CancelledError:
+        pass
     finally:
         for sig in signals:
             loop.remove_signal_handler(sig)
+        # `asyncio.run` will cancel and wait for all running tasks
+        # (including `main_task`) when leaving `main_coro()`
         logger.info("Waiting for tasks to clean up... (press CTRL+C to force quit)")
-        loop.call_later(CLEANUP_SECONDS, lambda *args: loop.stop())
-
-    # await asyncio.sleep(10)
-
-    # await throw_cancelled()
-    # await asyncio.sleep(1)
-
-    # task = asyncio.create_task(wait_shielded())
-    # task.cancel()
-    # print(task.cancelled())
-    # await asyncio.sleep(1)
-    # task.cancel()
-    # await asyncio.sleep(1)
-    # await task
-    # logger.info("Waiting for tasks to clean up... (press CTRL+C to force quit)")
-    # loop.call_later(CLEANUP_SECONDS, lambda *args: loop.stop())
+        loop.call_later(CLEANUP_SECONDS, force_quit)
 
 
 def main():
@@ -191,14 +111,7 @@ def main():
         format="%(name)s: %(message)s",
         level=logging.DEBUG if sys.flags.dev_mode else logging.INFO,
     )
-    # formatter = logging.Formatter("{funcName}: {message}", style="{")
-    # handler = logging.StreamHandler()
-    # handler.setFormatter(formatter)
-    # handler.setLevel(logging.DEBUG if sys.flags.dev_mode else logging.INFO)
-    # logger.addHandler(handler)
-    # logger.propagate = False
-    asyncio.run(main2())
-    print("After loop")
+    asyncio.run(main_coro())
 
 
 if __name__ == "__main__":
